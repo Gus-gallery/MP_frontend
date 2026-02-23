8e3aae6a53775aa4103bc8903762eea4605acfa5
Goose <207717976+Gus-gallery@users.noreply.github.com>
2026-02-23 13:37:53 +0100

    shopping bag functionality made better

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -19,24 +19,26 @@ export default function Cart({ onClose }) {
         <button className="hover:bg-zinc-400 cursor-pointer hover:text-2xl transition-all bg-zinc-500 rounded-full px-2" onClick={onClose}>✕</button>
       </div>
 
-      <div className="font-light text-md">
+      <div className="font-regular text-md">
         {items.length === 0
           ? <p className="font-bold">Empty Cart</p>
           : items.map(item => (
-            <div key={item.id} className="kurv-item">
-              <img src={item.image} alt={item.name} />
+            <div key={item.id} className="flex flex-col gap-4 aspect-square h-60 text-left">
+              <img className="rounded-md border-zinc-300 border object-cover aspect-square w-40" src={item.image} alt={item.name} />
               <div>
                 <strong>{item.name}</strong>
-                <p>{item.quantity} * {item.price} kr</p>
+                <div className="flex flex-row gap-2 items-center">
+                <p>{item.quantity} * {item.price} DKK.</p>
+                <button className="bg-red-900 rounded-full px-1.5 w-fit -mt-4" onClick={() => removeItem(item.id)}>✕</button>
+                </div>
               </div>
-              <button onClick={() => removeItem(item.id)}>✕</button>
             </div>
           ))
         }
       </div>
 
-      <div className="">
-        <p className="mb-2">Total: {total().toFixed(2)} kr</p>
+      <div className="items-left flex flex-col gap-4">
+        <p className="">Total: {total().toFixed(2)} DKK.</p>
         <button className="bg-zinc-700 hover:bg-zinc-800 transition-all px-2 cursor-pointer rounded-md py-1" onClick={gåTilCheckout} disabled={items.length === 0}>
           Go to payment
         </button>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState } from "react";
 import useCart from "../store/useCart";
 import Cart from './Cart';
+import { ShoppingBag } from 'lucide-react';
 
 const Header = () => {
     const [kurvAaben, setKurvAaben] = useState(false);
@@ -12,9 +13,16 @@ const Header = () => {
         <div section="header" className="p-2 px-4 items-center flex justify-between bg-zinc-700 mb-2 shadow-xl">
         <h1 className="font-black text-2xl">Project MarketPlace</h1>
 
-            <div className='flex items-center gap-4'>
-                <button className="text-xl font-bold" onClick={() => setKurvAaben(true)}>
-                Cart ({count()})
+            <div className='flex text-lg font-medium items-center gap-4'>
+                <h2>
+                    Artist Stories 
+                </h2>
+                <h2>
+                    Concept
+                </h2>
+
+                <button className="text-lg font-medium" onClick={() => setKurvAaben(true)}>
+                {count() == 0 ? <div className='bg-zinc-800 p-1 items-center hover:bg-zinc-900 transition-all rounded-md cursor-pointer'><ShoppingBag/></div> : <button className="bg-zinc-800 flex flex-row gap-2 items-center hover:bg-zinc-900 transition-all px-2 py-1 rounded-md cursor-pointer"><ShoppingBag/> {(count())}</button>} 
                 </button>
             </div>
         </div> 
