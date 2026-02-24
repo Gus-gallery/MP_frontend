f8cd15bbf46a59773f4f13c9484ea55b7d8cd7d0
Goose <207717976+Gus-gallery@users.noreply.github.com>
2026-02-24 11:32:46 +0100

    removed gradient in header

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,30 @@
-
+import { Link } from 'react-router-dom';
 
 const Hero = () => {
   return (
-    <div className="items-center flex flex-col container mx-auto">
-        <h1 className="font-bold text-3xl md:text-5xl lg:text-7xl text-center mt-2 mb-2">
+    <div className="items-center flex flex-col mx-auto py-20">
+        
+        
+        <h1 className="font-bold text-5xl lg:text-7xl text-center mt-2 mb-2">
             Market Project
         </h1>
-        <h2 className='font-light px-1 text-lg lg:text-xl text-center mb-2'>
+        <h2 className='font-light px-1 text-md lg:text-xl text-center mb-2'>
             A curated marketplace for items made with passion.
         </h2>
 
+        <div className="flex flex-row gap-20 mt-20">
+          <h3 className="font-bold text-3xl lg:text-5xl">
+            <Link to="/shop" className="px-4 py-2">
+              Shop
+            </Link>
+         </h3>
+          <h3 className='font-bold text-3xl lg:text-5xl'>
+            <Link to="/stories" className="px-4 py-2">
+              Stories
+            </Link>
+          </h3>
+        </div>
+      
     </div>
   )
 }
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -7,7 +7,6 @@ const LandingPage = () => {
     <header className="top-0 sticky z-10">
         <Header />
     </header>
-
     <Hero />
     </div>
   )
