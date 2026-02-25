89065f2b601f9fbb704823eb8ce5fe879c02bc97
Goose <207717976+Gus-gallery@users.noreply.github.com>
2026-02-25 12:08:24 +0100

    color change on thanks button

--- src/pages/Thanks.jsx
+++ src/pages/Thanks.jsx
@@ -3,7 +3,7 @@ export default function Tak() {
       <div className="flex flex-col items-center justify-center h-screen gap-4">
         <h1 className="text-3xl font-bold">Thanks for ordering!</h1>
         <p className="text-zinc-400 font-light">You will recieve a confirmation e-mail soon.</p>
-        <a href="/" className="bg-white font-light text-zinc-900 px-4 py-2 rounded-lg hover:bg-zinc-200 transition-colors">
+        <a href="/" className="bg-orange-900 font-medium text-zinc-200 px-4 py-2 rounded-lg hover:bg-orange-800 transition-colors">
           Back to shop.
         </a>
       </div>
