a14f9913f882b9afced36656602831e3195bd515
Goose <207717976+Gus-gallery@users.noreply.github.com>
2026-03-02 14:38:25 +0100

    small adjustments to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
         <header section="header" className="sticky top-0 z-10 p-2 px-4 items-center flex mb-2 min-h-15">
           <Link 
             to="/" 
-            className="font-regular text-sm absolute left-20 sm:left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-orange-400 opacity-80 px-2 text-center hover:opacity-100 transition-all lg:text-2xl"
+            className="font-regular text-md absolute left-23 sm:left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-orange-400 opacity-80 px-2 text-center hover:opacity-100 transition-all lg:text-2xl"
           >
             MARKET PROJECT
           </Link>
