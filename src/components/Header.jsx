2f5f98a5631f4d9384996e5e64023722d1725cb6
Goose <207717976+Gus-gallery@users.noreply.github.com>
2026-03-02 15:04:02 +0100

    complete colour scheme change - light mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,23 +14,23 @@ const Header = () => {
         <header section="header" className="sticky top-0 z-10 p-2 px-4 items-center flex mb-2 min-h-15">
           <Link 
             to="/" 
-            className="font-regular text-md absolute left-23 sm:left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-orange-400 opacity-80 px-2 text-center hover:opacity-100 transition-all lg:text-2xl"
+            className="font-regular text-md absolute left-23 sm:left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-neutral-900 opacity-80 px-2 text-center hover:opacity-100 transition-all lg:text-2xl"
           >
             MARKET PROJECT
           </Link>
           <div className='ml-auto flex items-center text-md md:text-lg font-light gap-4 lg:gap-8'>
-            <div className="flex items-center gap-8 lg:gap-12 text-sm md:text-lg font-light text-neutral-400">
-              <Link to="/stories" className="hover:underline hover:text-neutral-300 transition-all">
+            <div className="flex items-center gap-8 lg:gap-12 text-sm md:text-lg font-light text-neutral-600">
+              <Link to="/stories" className="hover:underline hover:text-neutral-800 transition-all">
                 ARTISANS
               </Link>
-              <Link to="/shop" className="hover:underline hover:text-neutral-300 transition-all">
+              <Link to="/shop" className="hover:underline hover:text-neutral-800 transition-all">
                 SHOP
               </Link>
               
               <div className="text-lg font-regular">
                 <button 
                   onClick={() => setKurvAaben(true)}
-                  className="flex flex-row gap-2 items-center hover:text-neutral-300 transition-all cursor-pointer"
+                  className="flex flex-row gap-2 items-center hover:text-neutral-800 transition-all cursor-pointer"
                 >
                   <ShoppingBag className='mb-1'/> {count}
                 </button>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className="items-center flex flex-col mx-auto py-20">
       <div className=''>
-        <video className="absolute top-0 left-0 w-full h-full opacity-50 object-cover" autoPlay playsInline loop muted>
+        <video className="absolute top-0 left-0 w-full h-full opacity-100 object-cover" autoPlay playsInline loop muted>
           <source src="/bg_vid.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
