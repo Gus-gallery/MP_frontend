0c95b14050362ab78796ff440be23bb022b1eb48
Goose <207717976+Gus-gallery@users.noreply.github.com>
2026-03-04 18:31:29 +0100

    no move on hover - product card

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
         <header section="header" className="sticky top-0 z-10 p-2 px-4 items-center flex mb-2 min-h-15">
           <Link 
             to="/" 
-            className="font-regular text-md absolute left-23 sm:left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-neutral-900 opacity-80 px-2 text-center hover:opacity-100 transition-all lg:text-2xl"
+            className="font-regular text-md absolute left-23 sm:left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-neutral-900 opacity-80 px-2 text-center hover:opacity-100 hover:text-orange-600 transition-all lg:text-2xl"
           >
             MARKET PROJECT
           </Link>
