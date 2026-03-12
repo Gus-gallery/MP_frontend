4139a05c6938ae40ecc8995ef7db75d163b672c6
Goose <207717976+Gus-gallery@users.noreply.github.com>
2026-03-12 23:30:52 +0100

    Katla with wide text and other visual nudges

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,20 +11,20 @@ const Header = () => {
   
     return (
       <div>
-        <header section="header" className="sticky bg-primary top-0 z-10 pt-1 px-6 items-center flex mb-2 min-h-18 shadow-lg">
+        <header section="header" className="sticky bg-primary top-0 z-10 pt-1 px-6 items-center flex mb-2 min-h-18 shadow-sm">
           <Link 
             to="/" 
-            className="font-regular hover:scale-105 text-md md:pt-1 text-neutral-900 opacity-90 px-2 text-center transition-all ease-in-out duration-200 md:text-xl"
+            className="font-bold tracking-wider hover:scale-105 text-md md:pt-1 text-sky-800 opacity-90 px-2 text-center transition-all ease-in-out duration-200 md:text-3xl"
           >
             KATLA
           </Link>
-          <div className='ml-auto flex items-center text-md md:text-lg font-light'>
-            <div className="flex items-center gap-6 md:gap-8 text-sm md:text-xl font-light text-neutral-600">
-              <Link to="/stories" className="hover:underline hover:text-neutral-800 transition-all">
-                Artisans
+          <div className='ml-auto tracking-wide flex items-center text-md md:text-lg font-light'>
+            <div className="flex items-center gap-6 md:gap-8 text-sm md:text-xl font-light text-neutral-500">
+              <Link to="/stories" className="hover:text-neutral-800 transition-all duration-300 ease-in-out">
+                artisans
               </Link>
-              <Link to="/shop" className="hover:underline hover:text-neutral-800 transition-all">
-                Shop
+              <Link to="/shop" className="hover:text-neutral-800 transition-all ease-in-out">
+                shop
               </Link>
               
               <div className={`text-lg font-regular ${count === 0 ? '-ml-6' : ''}`}>
