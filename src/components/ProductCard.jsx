8003409ceb714272d77706b0f4e470b164926773
Goose <207717976+Gus-gallery@users.noreply.github.com>
2026-03-13 22:13:14 +0100

    products updated with desciptions

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -5,11 +5,14 @@ export default function ProductCard({ product }) {
 
   return (
     <div className="flex flex-col gap-2">
-      <img className="border border-zinc-300 object-cover aspect-square w-full scale-100 transition-all ease-in-out duration-300" src={product.image} alt={product.name} />
-      <div className="border border-zinc-300 flex bg-primary flex-col p-4 items-center transition-all ease-in-out duration-300">
-        <h2 className="font-basker-medium text-neutral-800 text-left text-md">{product.name}</h2>
-        <p className="font-basker-regular text-neutral-800 text-md mb-4">{product.price} DKK.</p>
-        <button className="font-basker-bold text-lg hover:underline text-neutral-900 px-2 cursor-pointer transition-all" onClick={() => addItem(product)}>Add to cart</button>
+      <img className="object-cover aspect-square w-full scale-100 transition-all ease-in-out duration-300" src={product.image} alt={product.name} />
+      
+      
+      <div className="flex bg-white flex-col p-4 items-left transition-all ease-in-out duration-300">
+        <h2 className="font-basker-bold text-neutral-800 text-left text-lg">{product.name}</h2>
+        <p className="font-basker-medium text-neutral-800 text-md mb-4">{product.price} DKK.</p>
+        <p className="font-basker-regular text-neutral-800 text-md mb-4">{product.desription}</p>
+        <button className="font-basker-regular text-lg bg-zinc-600 border border-zinc-200 hover:underline text-primary px-2 cursor-pointer transition-all" onClick={() => addItem(product)}>Add to cart</button>
       </div>
     </div>
   );
