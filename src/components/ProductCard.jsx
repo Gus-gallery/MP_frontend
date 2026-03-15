97e6c579e62a8efa99fc717df1ad734cd78e8bdd
Goose <207717976+Gus-gallery@users.noreply.github.com>
2026-03-15 17:34:02 +0100

    productcard typo error

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -7,10 +7,12 @@ export default function ProductCard({ product }) {
     <div className="flex flex-col bg-primary">
       <img className="object-cover aspect-square w-full scale-100 transition-all ease-in-out duration-300" src={product.image} alt={product.name} />
       
-      <div className="flex flex-col p-4 items-left transition-all ease-in-out duration-300">
-        <h2 className="font-basker-bold text-neutral-800 text-left text-lg">{product.name}</h2>
-        <p className="font-basker-medium text-neutral-800 text-md mb-4">{product.price} DKK.</p>
-        <p className="font-basker-regular text-neutral-800 text-md mb-4">{product.desription}</p>
+      <div className="flex flex-col flex-1 justify-between p-4 items-left transition-all ease-in-out duration-300">
+        <div>
+          <h2 className="font-basker-bold text-neutral-800 text-left text-lg">{product.name}</h2>
+          <p className="font-basker-medium text-neutral-800 text-md mb-4">{product.price} DKK.</p>
+          <p className="font-basker-regular text-neutral-800 text-md mb-4">{product.description}</p>
+        </div>
         <button className="font-basker-regular text-lg bg-zinc-600 border border-zinc-200 hover:underline text-primary px-2 cursor-pointer transition-all" onClick={() => addItem(product)}>Add to cart</button>
       </div>
     </div>
--- src/products.js
+++ src/products.js
@@ -1,12 +1,12 @@
 const products = [
     { id: 1, name: "Hat", price: 350, image: "/products/hat.jpg",
-        desription: "The hat is a stylish and versatile accessory that can be worn for various occasions."
+        description: "The hat is a stylish and versatile accessory that can be worn for various occasions."
      },
     { id: 2, name: "Plasticana Shoes", price: 360, image: "/products/shoe.jpg",
-        desription: "The Plasticana shoe is a unique and innovative footwear option that combines style and sustainability."
+        description: "The Plasticana shoe is a unique and innovative footwear option that combines style and sustainability."
      },
     { id: 4, name: "gw.læder Belt", price: 800, image: "/products/belt.png",
-        desription: "The gw.læder belt is a handmade and durable accessory that is made in italian leather."
+        description: "The gw.læder belt is a handmade and durable accessory that is made in italian leather."
     },
   ];
   
