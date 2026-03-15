e88cf5ed3a552ef08383365e3c8e366d8773b1ae
Goose <207717976+Gus-gallery@users.noreply.github.com>
2026-03-15 17:23:58 +0100

    Hero updates - links on pictures

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,9 @@
+import { Link } from 'react-router-dom';
 
 const Hero = () => {
   return (
     <div className="flex flex-col p-8 h-screen">
+      <Link to="stories">
       <div className="border-b-2 border-zinc-300 shadow-md">
         <video className="absolute top-0 z-0 left-0 w-full h-screen opacity-100 object-cover" autoPlay playsInline loop muted>
           <source src="/bg_vid_4.mp4" type="video/mp4" />
@@ -9,11 +11,12 @@ const Hero = () => {
         </video>
       </div>   
 
-      <div  className="text-2xl md:text-3xl lg:text-4xl flex-col text-neutral-50 z-1 text-left md:max-w-2/3 xl:max-w-1/3 opacity-100 font-basker-medium -mt-2">
+      <div  className="text-2xl md:text-3xl lg:text-4xl flex-col text-neutral-50 z-1 absolute text-left max-w-2/3 xl:max-w-1/3 opacity-100 font-basker-medium -mt-2">
         <h1 className="mb-4">
             Katla celebrates crafts and culture, selling handmade products and telling stories.
         </h1>
       </div>
+      </Link>
     </div>
   )
 }
--- src/components/UnderHero.jsx
+++ src/components/UnderHero.jsx
@@ -6,23 +6,25 @@ const UnderHero = () => {
       <div className="relative grid grid-cols-1 md:grid-cols-2 font-basker-medium gap-2 w-full justify-center">  
         
         <div
-          className="relative h-120 bg-cover bg-center"
+          className="relative h-140 bg-cover bg-center"
           style={{ backgroundImage: "url('/assets/artisans.png')" }}
         >
+          <Link to="/artisans" className="absolute inset-0 z-10" />
           <div className="absolute inset-0 bg-black/30" />
-          <Link to="/stories" className="absolute p-8 text-primary text-2xl md:text-3xl lg:text-4xl">
+          <h2 className="absolute p-8 text-primary text-xl md:text-2xl lg:text-3xl">
             Read about the artisans
-          </Link>
+          </h2>
         </div>
 
         <div
-          className="relative h-120 bg-cover bg-center"
+          className="relative h-140 bg-cover bg-center"
           style={{ backgroundImage: "url('/assets/shop.png')" }}
         >
+          <Link to="/shop" className="absolute inset-0 z-10" />
           <div className="absolute inset-0 bg-black/30" />
-          <Link to="/shop" className="absolute p-8 text-primary text-2xl md:text-3xl lg:text-4xl">
+          <h2 to="/shop" className="absolute p-8 text-primary text-xl md:text-2xl lg:text-3xl">
             Explore the shop
-          </Link>
+          </h2>
         </div>
 
       </div>
