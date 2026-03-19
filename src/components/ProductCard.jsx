28b1d437d8735530f58136092018e23ace5a6e96
Goose <207717976+Gus-gallery@users.noreply.github.com>
2026-03-19 13:13:11 +0100

    border on product cards

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -7,7 +7,7 @@ export default function ProductCard({ product }) {
     <div className="flex flex-col bg-primary">
       <img className="object-cover aspect-square w-full scale-100 transition-all ease-in-out duration-300" src={product.image} alt={product.name} />
       
-      <div className="flex flex-col flex-1 justify-between p-4 items-left transition-all ease-in-out duration-300">
+      <div className="flex flex-col border border-neutral-400 flex-1 justify-between p-4 items-left transition-all ease-in-out duration-300">
         <div>
           <h2 className="font-basker-bold text-neutral-800 text-left text-lg">{product.name}</h2>
           <p className="font-basker-medium text-neutral-800 text-md mb-4">{product.price} DKK.</p>
