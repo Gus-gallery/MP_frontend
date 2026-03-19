532edf1c6cfe59d773190076d7abdf138ac7e599
Goose <207717976+Gus-gallery@users.noreply.github.com>
2026-03-19 13:11:18 +0100

    3 products on shop page

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -4,7 +4,7 @@ import products from '../products'
 
 const Products = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 justify-start mb-12 mt-2"> 
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 justify-start mb-12 mt-2"> 
         {products.map(p => <ProductCard key={p.id} product={p} />)}
     </div>
   )
